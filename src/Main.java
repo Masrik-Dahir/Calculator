24d2475b8885bf0901181e728340074beab236ac
Masrik Dahir <dahirma@vcu.edu>
2021-11-03 20:50:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        double value = (double) Func.factorial(10.0);
+//        System.out.println(value);
+        System.out.println(Func.sec(5));
+        System.out.println(Func.cosec(5));
+        System.out.println(Func.cot(5));
+        System.out.println(Func.sinh(5));
+        System.out.println(Func.cosh(5));
+        System.out.println(Func.tanh(5));
+        System.out.println(Func.sech(5));
+        System.out.println(Func.cosech(5));
+        System.out.println(Func.coth(5));
+//        System.out.println(Func.sec(5));
+//        System.out.println(Func.sec(5));
+//        System.out.println(Func.sec(5));
+        System.out.println(Func.permutation(10,2));
+        System.out.println(Func.combination(10,2));
+
+
+    }
+}
